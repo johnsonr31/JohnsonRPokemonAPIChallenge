41de957935c1daa420fcac5bcbb47a38935f734b
ricRobo4 <artistr3@proton.me>
2023-04-13 15:37:04 -0700

    * Able to get Type to Populate

--- src/functions.js
+++ src/functions.js
@@ -8,6 +8,9 @@ let savedInput = '';
 let searchURL = '';
 let searchBarInput = document.getElementById('searchBarInput');
 let pkmnSprite = document.getElementById('pkmnSprite');
+let pkmnName = document.getElementById('pkmnName');
+let pkmnType = document.getElementById('pkmnType');
+let pkmnLocation = document.getElementById('pkmnLocation');
 
 function saveToFavoritesByName(name){
     let favorites = [];
@@ -26,13 +29,18 @@ function GetRandomMon()
     
     fetch(`https://pokeapi.co/api/v2/pokemon/${radNum}/`)
     .then((response) => response.json())
-    .then((data) => pkmnName.innerHTML = data.name);
+    .then((data) => {pkmnName.innerHTML = data.name
+                    pkmnType.innerHTML = data.types[0].type.name
+    });
 }
 
 async function SearchAPI(url)
 {
     await fetch(url).then(response => response.json())
-    .then((data) => pkmnName.innerHTML = data.name);
+    .then((data) => {pkmnName.innerHTML = data.name
+                    pkmnType.innerHTML = data.types[0].type.name
+                    // pkmnLocation.innerHTML = data.location_area_encounters
+    });
 }
 
 function SearchPokemon()
